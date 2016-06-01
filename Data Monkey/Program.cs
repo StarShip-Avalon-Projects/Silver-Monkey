67eb3e409d50ee8ac9c086f1e07e71f3b6e496e0
gerolkaae@hotmail.com <gerolkae@hotmail.com>
2016-06-01 14:32:16 -0700

    Cleanup and nuGet added for SQLite and Fast Colored Textbox

---
+++ Data Monkey/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SQLiteEditor
+{
+	public class Program
+	{
+		/// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        { 
+/*        	
+        	String appropriateFile = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath),"DLLx64" + Path.DirectorySeparatorChar + "System.Data.SQLite.dll");
+  			if(!Environment.Is64BitOperatingSystem)
+  				appropriateFile = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath),"DLLx86" + Path.DirectorySeparatorChar + "System.Data.SQLite.dll");
+        	Assembly.LoadFrom(appropriateFile);
+*/
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SQLiteEditor.frmExplorer()) ;
+           
+        }
+	}
+
+}
+
--- SilverMonkey/Engine/MSPK_MDB.vb
+++ SilverMonkey/Engine/MSPK_MDB.vb
@@ -25,16 +25,7 @@ Public Class MSPK_MDB
     Dim lock As New Object
     Dim cache As Dictionary(Of String, Object) = New Dictionary(Of String, Object)
     
-    Public Shared Sub LoadSQLLiteAssembly()
-  		Dim appropriateFile As String = Path.Combine(My.Application.Info.DirectoryPath,"DLLx64" + Path.DirectorySeparatorChar + "System.Data.SQLite.dll")
-   		If Not Environment.Is64BitOperatingSystem Then
-   			appropriateFile = Path.Combine(My.Application.Info.DirectoryPath,"DLLx86" + Path.DirectorySeparatorChar + "System.Data.SQLite.dll")
-   		End If
-        Assembly.LoadFrom(appropriateFile)
-    End sub
     Public Sub New()
-    	
-    	LoadSQLLiteAssembly()
     	
         Try
             writer = New TextBoxWriter(Variables.TextBox1)
