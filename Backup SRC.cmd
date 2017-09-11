f17c358ce64de707ea7c8a29ac4f0fec3d298e7d
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-11 02:30:19 -0700

    Auto Version Update

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -14,12 +14,11 @@ msbuild /t:BuildAll  Solution.build
 :End
 git add --all
 git commit -m"Auto Version Update" --all
-git push
 
-
+git submodule foreach 'git add --all' ;
 git submodule foreach 'git commit -a' ;
      git submodule foreach 'git push --all' ;
-     git commit -a && \
+
      git push --all --recurse-submodules=on-demand
 
 git request-pull master https://github.com/StarShip-Avalon-Projects/Silver-Monkey.git
