44363d7a8c3ffee518ed5400d623aaab362eee99
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-11 00:55:22 -0700

    Update docs

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -1,2 +1,17 @@
-msbuild /t:BuildAll /fl /flp:logfile=MyProjectOutput.log;verbosity=diagnostic  Solution.build
-pause
+
+git pull --all
+IF "%~1"=="" GOTO BuildAll
+IF "%~1"=="VersionBump" GOTO VersionBump
+
+:VersionBump
+msbuild /t:IncrementVersions;BuildAll  Solution.build
+goto :End
+
+:BuildAll
+msbuild /t:BuildAll  Solution.build
+
+:End
+git add --all
+git commit -m"Update docs" --all
+git push
+
