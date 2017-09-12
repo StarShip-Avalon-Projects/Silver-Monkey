a529500f716f6b4e9cf913c7f2035242f7837462
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-12 12:05:42 -0700

    Auto Version Update

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -1,6 +1,16 @@
-
+@echo off
 git pull
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 git submodule update -f --merge
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+git submodule foreach "git pull"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 IF "%~1"=="" GOTO BuildAll
 IF "%~1"=="VersionBump" GOTO VersionBump
 
@@ -22,13 +32,29 @@ exit /b 1
 
 :End
 git add --all
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 git commit -m"Auto Version Update" --all
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
 
 
 git submodule foreach "git add --all"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 git submodule foreach "git commit -m'Auto Update SubModules'-a"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 git submodule foreach "git push -f origin HEAD:master"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
 git push -f --all --recurse-submodules=on-demand
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
 
 git request-pull v2.19.x_Elta https://github.com/StarShip-Avalon-Projects/Silver-Monkey.git
 
