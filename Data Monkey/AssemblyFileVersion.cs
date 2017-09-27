a41abef0799fbe0f772261eeb788f4a6fe00979b
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-27 12:26:21 -0700

    Auto Version Update

--- Data Monkey/AssemblyFileVersion.cs
+++ Data Monkey/AssemblyFileVersion.cs
@@ -5,6 +5,6 @@
 // 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.904.0926")]
-[assembly: AssemblyFileVersion("1.1.904.0926")]
+[assembly: AssemblyVersion("1.1.904.0927")]
+[assembly: AssemblyFileVersion("1.1.904.0927")]
 
