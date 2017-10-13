d5d7b2dc1a6cb97ebcb759c841fc36491e5dfe9e
Jeremy Trombley <gerolkae@hotmail.com>
2017-10-13 08:02:25 -0700

    Auto Version Update

--- UpDateSrc.cmd
+++ UpDateSrc.cmd
@@ -3,9 +3,6 @@ git fetch --recurse-submodules=on-demand
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto fail 
 
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto fail
-
 :eof
 exit /b 0
 
