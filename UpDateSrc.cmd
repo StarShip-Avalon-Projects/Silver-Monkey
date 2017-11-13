3246a198e95134538466d9ae841e6ca19fdba009
Jeremy Trombley <gerolkae@hotmail.com>
2017-11-13 01:57:59 -0700

    Auto Version Update

--- UpDateSrc.cmd
+++ UpDateSrc.cmd
@@ -1,11 +1,18 @@
 
-git pull --recurse-submodules=on-demand
+git.exe pull --recurse-submodules=on-demand
+
 set GIT_STATUS=%ERRORLEVEL% 
+
 if not %GIT_STATUS%==0 goto fail 
 
+
 :eof
+
 exit /b 0
 
+
 :fail 
+
 pause 
-exit /b 1
+
+:exit /b 1
