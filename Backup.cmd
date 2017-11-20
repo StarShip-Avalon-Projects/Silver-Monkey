448e4b1549bac693f75854a2c619d2abc3d57265
Jeremy Trombley <gerolkae@hotmail.com>
2017-11-19 22:49:52 -0700

    Auto Version Update

--- Backup.cmd
+++ Backup.cmd
@@ -4,11 +4,15 @@ IF "%~1"=="" GOTO BuildAll
 IF "%~1"=="VersionBump" GOTO VersionBump
 
 :VersionBump
-call MsBuildSolution.cmd "VersionBump"
+call MsBuildSolution.cmd VersionBump
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto eof 
 goto End
 
 :BuildAll
 call MsBuildSolution.cmd
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto eof 
 
 :End
 git add --all
