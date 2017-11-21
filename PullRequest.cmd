00aea001de112c71c322d6b6faa5d6ce85ab2357
Jeremy Trombley <gerolkae@hotmail.com>
2017-11-20 17:04:53 -0700

    Auto Version Update

--- PullRequest.cmd
+++ PullRequest.cmd
@@ -1,2 +1,2 @@
 :pull
-git request-pull v2.19.x_Elta https://github.com/StarShip-Avalon-Projects/Silver-Monkey.git v2.19.x.Elta
+git request-pull PhoenxSpeakBeta https://github.com/StarShip-Avalon-Projects/Silver-Monkey.git PhoenxSpeakBeta
