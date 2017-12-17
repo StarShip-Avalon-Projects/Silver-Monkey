c58981d0cbd3cd99e1a4fca35fdf0af1f3e32475
Jeremy Trombley <gerolkae@hotmail.com>
2017-12-17 16:13:10 -0700

    Auto Version Update

--- Send Update.cmd
+++ Send Update.cmd
@@ -1,21 +1,16 @@
 
 git.exe add --all
 
-git submodule init
-git submodule update
-
 git.exe submodule foreach "git.exe add --all"
 set git.exe_STATUS=%ERRORLEVEL% 
 if not %git.exe_STATUS%==0 goto eof 
 
-git.exe submodule foreach "git.exe commit -m'Auto Update SubModules' || true"
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto eof 
+git.exe submodule foreach "git.exe commit -m'Auto Update SubModules'"
 git.exe commit -m"Auto Version Update"
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto eof 
 
-git.exe push --recurse-submodules=on-demand
+git.exe push --recurse-submodules=yes
 set git.exe_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto eof
 
