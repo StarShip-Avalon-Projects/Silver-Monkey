efd1b3898bc4dc831e311a14b3b6a4ee3d78cc82
Jeremy Trombley <gerolkae@hotmail.com>
2017-12-26 00:50:58 -0700

    Initial SM-wpf GUI Layout

---
+++ SilverMonkey/Silver Monkey 3/ViewModels/ShellViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Caliburn.Micro;
+using Furcadia.Net.DreamInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilverMonkey.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        #region Private Fields
+
+        private string _LogOutputBox;
+        private string _InputTextBox;
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the log output box.
+        /// </summary>
+        /// <value>
+        /// The log output box.
+        /// </value>
+        public string LogOutputBox
+        {
+            get { return _LogOutputBox; }
+            set
+            {
+                _LogOutputBox = value;
+                NotifyOfPropertyChange(() => LogOutputBox);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the input text box.
+        /// </summary>
+        /// <value>
+        /// The input text box.
+        /// </value>
+        public string InputTextBox
+        {
+            get { return _InputTextBox; }
+            set
+            {
+                _InputTextBox = value;
+                NotifyOfPropertyChange(() => InputTextBox);
+            }
+        }
+
+        private BindableCollection<Furre> _FurreList = new BindableCollection<Furre>();
+        public Furre SelectedFurre { get; set; }
+
+        #endregion Public Properties
+    }
+}
