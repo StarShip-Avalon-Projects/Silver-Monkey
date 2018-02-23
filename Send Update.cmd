53e611cf8a0de13acdd9d110cfaca3ab965e38a9
Jeremy Trombley <gerolkae@hotmail.com>
2018-02-22 18:56:55 -0700

    pft

--- Send Update.cmd
+++ Send Update.cmd
@@ -5,7 +5,7 @@ rem set git.exe_STATUS=%ERRORLEVEL%
 rem if not %git.exe_STATUS%==0 goto eof 
 
 git.exe submodule foreach "git.exe commit -am'Auto Update SubModules' || true"
-git commit --amend --no-edit
+git commit -am'Auto Update SubModules'
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto eof 
 
