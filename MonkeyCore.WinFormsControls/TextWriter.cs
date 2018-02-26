7dbdf0c8173bcc1607c537baed192ae728a302b7
Jeremy Trombley <gerolkae@hotmail.com>
2018-02-25 20:59:31 -0700

    Move MonkeyCore.Utils.VB to  Silvermonkey.Liraries.Utils C#

---
+++ MonkeyCore.WinFormsControls/TextWriter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Controls
+{
+    /// <summary>
+    ///
+    /// </summary>
+    /// <seealso cref="System.IO.TextWriter" />
+    public class TextBoxWriter : TextWriter
+    {
+        private StringBuilder Builder;
+
+        private TextBoxBase control;
+
+        public TextBoxWriter(TextBox control)
+        {
+            this.control = control;
+            control.HandleCreated += OnHandleCreated;
+            Builder = new StringBuilder();
+        }
+
+        public TextBoxWriter(RichTextBox control)
+        {
+            this.control = control;
+            control.HandleCreated += OnHandleCreated;
+        }
+
+        public delegate void AppendTextDelegate(string Text);
+
+        public override Encoding Encoding
+        {
+            get
+            {
+                return Encoding.Default;
+            }
+        }
+
+        public override void Write(char ch)
+        {
+            this.Write(ch.ToString());
+        }
+
+        public override void Write(string s)
+        {
+            if (control.IsHandleCreated)
+            {
+                this.AppendText(s);
+            }
+            else
+            {
+                this.BufferText(ref s);
+            }
+        }
+
+        public override void WriteLine(string s)
+        {
+            this.Write(s + Environment.NewLine);
+        }
+
+        private void AppendText(string Text)
+        {
+            if (control.InvokeRequired)
+            {
+                AppendTextDelegate d = new AppendTextDelegate(AppendText);
+                control.Invoke(d, Text);
+            }
+            else
+            {
+                if (Builder.Length > 0)
+                {
+                    control.AppendText(Builder.ToString());
+                    Builder.Clear();
+                }
+
+                control.AppendText(Text);
+                //  (ByRef lb As Object, ByRef obj As Object, ByRef newColor As fColorEnum)
+                // Main.AddDataToList(Main.log_, s, 0)
+            }
+        }
+
+        private void BufferText(ref string s)
+        {
+            Builder.Append(s);
+        }
+
+        private void OnHandleCreated(object sender, EventArgs e)
+        {
+            if (Builder.Length > 0)
+            {
+                control.AppendText(Builder.ToString());
+                Builder.Clear();
+            }
+        }
+    }
+}
---
+++ SilverMonkey.EnginLibrariesCs/Extentions/Utils.cs
@@ -0,0 +1,129 @@
+﻿/// <summary>
+/// General Utility functions we haven't found a home for yet
+/// </summary>
+using System;
+
+namespace Extentions
+{
+    /// <summary>
+    /// General Utilities with Phoenix Speak in mind
+    /// </summary>
+    public sealed class Utilities
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Convert DateTime to Unixtimestamp
+        /// </summary>
+        /// <param name="dTime">The Specified DataTime</param>
+        /// <returns></returns>
+        public static double DateTimeToUnixTimestamp(DateTime dTime)
+        {
+            return ((DateTimeOffset)dTime).ToUnixTimeSeconds();
+        }
+
+        /// <summary>
+        /// Letter increment for walking PS tables
+        /// </summary>
+        /// <param name="Input"></param>
+        /// <returns></returns>
+        public static char IncrementLetter(string Input)
+        {
+            Input = Input.Substring(0, 1);
+            int i = Input[0];
+            char test;
+            switch (Input)
+            {
+                case "A":
+                case "B":
+                case "C":
+                case "D":
+                case "E":
+                case "F":
+                case "G":
+                case "H":
+                case "I":
+                case "J":
+                case "K":
+                case "L":
+                case "M":
+                case "N":
+                case "O":
+                case "P":
+                case "Q":
+                case "R":
+                case "S":
+                case "T":
+                case "U":
+                case "V":
+                case "W":
+                case "X":
+                case "Y":
+                case "Z":
+
+                case "a":
+                case "b":
+                case "c":
+                case "d":
+                case "e":
+                case "f":
+                case "g":
+                case "h":
+                case "i":
+                case "j":
+                case "k":
+                case "l":
+                case "m":
+                case "n":
+                case "o":
+                case "p":
+                case "q":
+                case "r":
+                case "s":
+                case "t":
+                case "u":
+                case "v":
+                case "w":
+                case "x":
+                case "y":
+                case "z":
+
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                    test = (char)(i + 1);
+                    break;
+
+                case "9":
+                    test = 'a';
+                    break;
+
+                default:
+                    test = '{';
+                    break;
+            }
+            return test;
+        }
+
+        /// <summary>
+        /// Converts a number representing a Unix Time stamp and converts it to a usable DateTime format
+        /// </summary>
+        /// <param name="unixTimeStamp"></param>
+        /// <returns>DataTime</returns>
+        public static DateTime UnixTimeStampToDateTime(ref double unixTimeStamp)
+        {
+            //  Unix timestamp is seconds past epoch
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToUniversalTime();
+            return dtDateTime;
+        }
+
+        #endregion Public Methods
+    }
+}
