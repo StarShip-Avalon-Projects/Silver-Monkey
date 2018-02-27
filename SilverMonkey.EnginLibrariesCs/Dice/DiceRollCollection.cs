152c9ce45201fb82266c16bd6a1ea83da9bb86d6
Jeremy Trombley <gerolkae@hotmail.com>
2018-02-27 12:16:28 -0700

    Move Dice to SilverMonkey.EnginLibrariesCs

---
+++ SilverMonkey.EnginLibrariesCs/Dice/DiceRollCollection.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Libraries.Dice
+{
+    /// <summary>
+    /// Grab your dice and shake them till you're ready to roll
+    /// </summary>
+    public class DiceRollCollection : System.Collections.ObjectModel.Collection<Die>
+    {
+        private List<Die> Dice = new List<Die>();
+        private double Offset;
+        private char DiceModifyer = '+';
+
+        /// <summary>
+        /// Mix up the Dice in hand and roll them
+        /// </summary>
+        /// <returns>
+        /// Sum of the result as <see cref="Double"/>
+        /// </returns>
+        public double RollAll()
+        {
+            double total = 0;
+            foreach (Die die in Dice)
+            {
+                total += die.Roll();
+            }
+            if (DiceModifyer == '+')
+                total += Offset;
+            else if (DiceModifyer == '-')
+                total -= Offset;
+            return total;
+        }
+
+        public DiceRollCollection(double NumberDice, double NumberSides, double Offset = 0, char Modifyer = '+')
+        {
+            this.DiceModifyer = Modifyer;
+            this.Offset = Offset;
+
+            for (double I = 0; I <= NumberDice - 1; I++)
+            {
+                Dice.Add(new Die(NumberSides));
+            }
+        }
+    }
+}
---
+++ SilverMonkey.EnginLibrariesCs/Dice/Die.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Libraries.Dice
+{
+    /// <summary>
+    /// Single Die object for Silver Monkey generating a Dice Roll
+    /// </summary>
+    public class Die
+    {
+        private static Random faceSelector = new Random();
+
+        private double faceCount;
+
+        private double value;
+
+        public Die(double faceCount)
+        {
+            if (faceCount < 1)
+            {
+                throw new ArgumentOutOfRangeException("faceCount", "Dice must have one or more faces.");
+            }
+
+            this.faceCount = faceCount;
+        }
+
+        public double FaceCount
+        {
+            get
+            {
+                return faceCount;
+            }
+            set
+            {
+                faceCount = value;
+            }
+        }
+
+        public double Value
+        {
+            get
+            {
+                return value;
+            }
+        }
+
+        /// <summary>
+        /// Roll a single die
+        /// </summary>
+        /// <returns>
+        /// <see cref="Double"/>
+        /// </returns>
+        public double Roll()
+        {
+            value = faceSelector.Next(1, (int)FaceCount);
+            return Value;
+        }
+    }
+}
